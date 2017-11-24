74baa37e2f2e01ab95075c5c273d930708eaa6b0
venki <vr.com>
2017-11-24 11:31:30 +0530

    Just Update

---
+++ SpringMVC_JDBC_Rest_Configuration/src/main/java/com/spring/client/ExampleClient.java
@@ -0,0 +1,10 @@
+package com.spring.client;
+
+public class ExampleClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- SpringMVC_JDBC_Rest_Configuration/src/main/java/com/spring/server/ExampleController.java
+++ SpringMVC_JDBC_Rest_Configuration/src/main/java/com/spring/server/ExampleController.java
@@ -1,4 +1,4 @@
-package com.spring.controller;
+package com.spring.server;
 
 import java.util.ArrayList;
 import java.util.List;
