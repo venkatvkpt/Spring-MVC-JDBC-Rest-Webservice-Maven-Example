41df797577403399f9e61d1b60b1d572e8c71591
Venki <venkatkcpd@gmail.com>
2017-11-24 11:47:03 +0530

    Update client response.

--- SpringMVC_JDBC_Rest_Configuration/src/main/java/com/spring/client/ExampleClient.java
+++ SpringMVC_JDBC_Rest_Configuration/src/main/java/com/spring/client/ExampleClient.java
@@ -1,9 +1,28 @@
 package com.spring.client;
 
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class ExampleClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final String uri = "http://localhost:8082/SpringMVC_JDBC_Rest_Configuration/examples";
+	     
+	    RestTemplate restTemplate = new RestTemplate();
+	     
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+	     
+	    ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+	     
+	    System.out.println(result.getBody());
 
 	}
 
